a0f1900fe6cbd645dd6e8b635f6fabea8ff276a1
leejinwoo <leejinwoo1126@gmail.com>
2023-12-17 18:30:32 +0900

    feat: JPA Repository, JpaConfig, 설정 변경

--- src/main/java/hello/itemservice/domain/Item.java
+++ src/main/java/hello/itemservice/domain/Item.java
@@ -2,13 +2,28 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 @Data
+@Entity
+@Table
 public class Item {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "item_name", length = 10)
     private String itemName;
+
+    @Column
     private Integer price;
+
+    @Column
     private Integer quantity;
 
     public Item() {
